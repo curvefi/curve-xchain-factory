e2855e6a32e005d2dd65b176cd2e08a285943657
Edward Amor <edward.amor3@gmail.com>
2021-09-25 01:01:41 -0400

    feat(test): add mock child gauge fixtures

--- tests/fixtures/deployments.py
+++ tests/fixtures/deployments.py
@@ -9,3 +9,13 @@ def root_factory(alice, RootChainGaugeFactory):
 @pytest.fixture(scope="module")
 def mock_root_gauge_implementation(alice, MockRootGauge):
     return MockRootGauge.deploy({"from": alice})
+
+
+@pytest.fixture(scope="module")
+def child_factory(alice, ChildChainGaugeFactory):
+    return ChildChainGaugeFactory.deploy({"from": alice})
+
+
+@pytest.fixture(scope="module")
+def mock_child_gauge_implementation(alice, MockChildGauge):
+    return MockChildGauge.deploy({"from": alice})
