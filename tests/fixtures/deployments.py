ea1b066e2fed63da9a61ed1a4cf313f866b7f991
Edward Amor <edward.amor3@gmail.com>
2021-09-24 20:42:03 -0400

    feat(test): add mock root gauge implementation fixture

--- tests/fixtures/deployments.py
+++ tests/fixtures/deployments.py
@@ -4,3 +4,8 @@ import pytest
 @pytest.fixture(scope="module")
 def root_chain_factory(alice, RootChainGaugeFactory):
     return RootChainGaugeFactory.deploy({"from": alice})
+
+
+@pytest.fixture(scope="module")
+def mock_root_gauge_implementation(alice, MockRootGauge):
+    return MockRootGauge.deploy({"from": alice})
