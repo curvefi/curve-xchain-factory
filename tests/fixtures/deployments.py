dba1b89a1f466cf5ec5eb86a54dc59684dfc48bc
Edward Amor <edward.amor3@gmail.com>
2021-09-26 01:16:18 -0400

    feat(test): add fixture infrastructure for testing root gauge instances

--- tests/fixtures/deployments.py
+++ tests/fixtures/deployments.py
@@ -1,5 +1,11 @@
 import pytest
+from brownie import ETH_ADDRESS, compile_source
 from brownie_tokens import ERC20
+
+
+@pytest.fixture(scope="session")
+def dao_contracts(pm):
+    return pm("curvefi/curve-dao-contracts@1.3.0")
 
 
 @pytest.fixture(scope="module")
@@ -30,3 +36,40 @@ def mock_child_streamer_implementation(alice, MockChildStreamer):
 @pytest.fixture(scope="module")
 def child_streamer_implementation(alice, ChildChainStreamer):
     return ChildChainStreamer.deploy({"from": alice})
+
+
+@pytest.fixture(scope="module")
+def token(alice, dao_contracts):
+    return dao_contracts.ERC20CRV.deploy("Curve DAO Token", "CRV", 18, {"from": alice})
+
+
+@pytest.fixture(scope="module")
+def voting_escrow(alice, token, dao_contracts):
+    return dao_contracts.VotingEscrow.deploy(
+        token, "Voting-escrowed CRV", "veCRV", "veCRV", {"from": alice}
+    )
+
+
+@pytest.fixture(scope="module")
+def gauge_controller(alice, dao_contracts, token, voting_escrow):
+    return dao_contracts.GaugeController.deploy(token, voting_escrow, {"from": alice})
+
+
+@pytest.fixture(scope="module")
+def minter(alice, chain, dao_contracts, gauge_controller, token):
+    _minter = dao_contracts.Minter.deploy(token, gauge_controller, {"from": alice})
+
+    token.set_minter(_minter, {"from": alice})
+    chain.mine(timedelta=604800)
+    token.update_mining_parameters({"from": alice})
+
+    return _minter
+
+
+@pytest.fixture(scope="module")
+def anyswap_root_gauge_implementation(alice, minter, RootGaugeAnyswap):
+    source = RootGaugeAnyswap._build["source"]
+    for value in [minter.address, ETH_ADDRESS]:
+        source = source.replace("ZERO_ADDRESS", value, 1)
+    NewRootGaugeAnyswap = compile_source(source).Vyper
+    return NewRootGaugeAnyswap.deploy({"from": alice})
