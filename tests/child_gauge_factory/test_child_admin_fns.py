e927639911acadce2ef36d5e473a7a4436970c4b
Edward Amor <edward.amor3@gmail.com>
2021-12-03 07:45:58 +0000

    test: guarded admin fns child gauge factory

--- tests/child_gauge_factory/test_child_admin_fns.py
+++ tests/child_gauge_factory/test_child_admin_fns.py
@@ -13,3 +13,55 @@ def test_set_anycall(alice, child_gauge_factory):
 def test_set_anycall_guarded(bob, child_gauge_factory):
     with brownie.reverts():
         child_gauge_factory.set_anycall(ETH_ADDRESS, {"from": bob})
+
+
+def test_set_permitted(alice, child_gauge_factory):
+    tx = child_gauge_factory.set_permitted(ETH_ADDRESS, True)
+
+    assert child_gauge_factory.permitted(ETH_ADDRESS) is True
+    assert "UpdatePermission" in tx.events
+    assert tx.events["UpdatePermission"].values() == [ETH_ADDRESS, True]
+
+
+def test_set_permitted_guarded(bob, child_gauge_factory):
+    with brownie.reverts():
+        child_gauge_factory.set_permitted(ETH_ADDRESS, True, {"from": bob})
+
+
+def test_set_manager(alice, child_gauge_factory):
+    tx = child_gauge_factory.set_manager(ETH_ADDRESS, {"from": alice})
+
+    assert child_gauge_factory.manager() == ETH_ADDRESS
+    assert "ManagerUpdated" in tx.events
+    assert tx.events["ManagerUpdated"].values() == [alice, ETH_ADDRESS]
+
+
+def test_set_manager_guarded(bob, child_gauge_factory):
+    with brownie.reverts():
+        child_gauge_factory.set_manager(ETH_ADDRESS, {"from": bob})
+
+
+def test_set_voting_escrow(alice, child_gauge_factory):
+    tx = child_gauge_factory.set_voting_escrow(ETH_ADDRESS, {"from": alice})
+
+    assert child_gauge_factory.voting_escrow() == ETH_ADDRESS
+    assert "UpdateVotingEscrow" in tx.events
+    assert tx.events["UpdateVotingEscrow"].values() == [ZERO_ADDRESS, ETH_ADDRESS]
+
+
+def test_set_voting_escrow_guarded(bob, child_gauge_factory):
+    with brownie.reverts():
+        child_gauge_factory.set_voting_escrow(ETH_ADDRESS, {"from": bob})
+
+
+def test_set_implementation(alice, child_gauge_factory):
+    tx = child_gauge_factory.set_implementation(ETH_ADDRESS, {"from": alice})
+
+    assert child_gauge_factory.get_implementation() == ETH_ADDRESS
+    assert "UpdateImplementation" in tx.events
+    assert tx.events["UpdateImplementation"].values() == [ZERO_ADDRESS, ETH_ADDRESS]
+
+
+def test_set_implementation_guarded(bob, child_gauge_factory):
+    with brownie.reverts():
+        child_gauge_factory.set_implementation(ETH_ADDRESS, {"from": bob})
