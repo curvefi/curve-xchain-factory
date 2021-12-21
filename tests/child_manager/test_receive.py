b52876b0e920ab19cce6c935e4733f0f55a34333
Edward Amor <edward.amor3@gmail.com>
2021-12-21 17:18:19 +0000

    fix: rm receive ve data child manager tests

--- tests/child_manager/test_receive.py
+++ tests/child_manager/test_receive.py
@@ -13,33 +13,3 @@ def test_deploy_gauge_successful(
 def test_deploy_gauge_unsuccessful(alice, lp_token, child_manager):
     with brownie.reverts():
         child_manager.deploy_gauge(lp_token, 0x0, alice, {"from": alice})
-
-
-def test_receive_ve_data_successful(alice, chain, anycall, child_manager):
-    tx = child_manager.receive_ve_data(
-        alice,
-        100 * 10 ** 18,
-        chain.time() + 86400 * 365 * 4,
-        100 * 10 ** 18,
-        chain.time(),
-        {"from": anycall},
-    )
-
-    assert "UpdateVEBalance" in tx.events
-    assert "UpdateVETotalSupply" in tx.events
-
-    assert child_manager.totalSupply() <= 100 * 10 ** 18
-    assert child_manager.balanceOf(alice) <= 100 * 10 ** 18
-    assert child_manager.locked__end(alice) >= tx.timestamp + 86400 * 365 * 4
-
-
-def test_receive_ve_data_unsuccessful(alice, chain, child_manager):
-    with brownie.reverts():
-        child_manager.receive_ve_data(
-            alice,
-            100 * 10 ** 18,
-            chain.time() + 86400 * 365 * 4,
-            100 * 10 ** 18,
-            chain.time(),
-            {"from": alice},
-        )
