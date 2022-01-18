2aa8c63ecb50ec639aa5e31deb84ebe6cdefeb02
Edward Amor <edward.amor3@gmail.com>
2022-01-18 17:38:24 +0000

    feat: finish example proof generation/verification script

--- scripts/proof.py
+++ scripts/proof.py
@@ -1,6 +1,7 @@
 import rlp
 from brownie import web3
 from hexbytes import HexBytes
+from trie import HexaryTrie
 
 VOTING_ESCROW = "0x5f3b5DfEb7B28CDbD7FAba78963EE202a494e2A2"
 BLOCK_NUMBER = 14000000
@@ -29,6 +30,9 @@ HEADER_ORDER = (
 def main():
     # fetch the block of interest
     block = web3.eth.get_block(BLOCK_NUMBER)
+    # fetch the proof
+    proof = web3.eth.get_proof(VOTING_ESCROW, [0], BLOCK_NUMBER)
+
     # filter only the fields needed to replicate the block hash
     header_fields = {k: block[k] for k in HEADER_ORDER}
 
@@ -44,3 +48,24 @@ def main():
     rlp_input = rlp.encode(list(header_fields.values()))
     # assert the blockhash calculated matches the blockhash on-chain
     assert block["hash"] == web3.keccak(rlp_input)
+    state_root = block["stateRoot"]  # root hash of the global state trie
+
+    # get the account rlp [nonce, balance, storageHash, codehash]
+    account_rlp = HexaryTrie.get_from_proof(
+        state_root,
+        web3.keccak(HexBytes(VOTING_ESCROW)),
+        list(map(rlp.decode, proof["accountProof"])),
+    )
+    storage_root = rlp.decode(account_rlp)[2]  # root hash of the storage trie for the account
+    assert storage_root == proof["storageHash"]
+
+    # get the value in storage
+    storage_proof = proof["storageProof"][0]
+    value = HexaryTrie.get_from_proof(
+        storage_root,
+        # the path for a storage value is the hash of its slot
+        web3.keccak(HexBytes((0).to_bytes(32, "big"))),
+        list(map(rlp.decode, storage_proof["proof"])),
+    )
+    # decode the value
+    assert value == rlp.encode(storage_proof.value)
