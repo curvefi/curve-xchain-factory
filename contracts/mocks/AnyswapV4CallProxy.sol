e2a7a280ba07d69d78bc8672a4f5fe1c4900fb06
Edward Amor <edward.amor3@gmail.com>
2022-02-05 03:40:58 +0000

    chore: update mock anyswap call proxy

--- contracts/mocks/AnyswapV4CallProxy.sol
+++ contracts/mocks/AnyswapV4CallProxy.sol
@@ -1,232 +1,245 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-
 pragma solidity ^0.8.6;
 
-// MPC management means multi-party validation.
-// MPC signing likes Multi-Signature is more secure than use private key directly.
-abstract contract MPCManageable {
-    address public mpc;
-    address public pendingMPC;
-
-    uint256 public constant delay = 2 days;
-    uint256 public delayMPC;
-
-    modifier onlyMPC() {
-        require(msg.sender == mpc, "MPC: only mpc");
-        _;
-    }
-
-    event LogChangeMPC(
-        address indexed oldMPC,
-        address indexed newMPC,
-        uint256 effectiveTime);
-
-    event LogApplyMPC(
-        address indexed oldMPC,
-        address indexed newMPC,
-        uint256 applyTime);
-
-    constructor(address _mpc) {
-        require(_mpc != address(0), "MPC: mpc is the zero address");
-        mpc = _mpc;
-        emit LogChangeMPC(address(0), mpc, block.timestamp);
-    }
-
-    function changeMPC(address _mpc) external onlyMPC {
-        require(_mpc != address(0), "MPC: mpc is the zero address");
-        pendingMPC = _mpc;
-        delayMPC = block.timestamp + delay;
-        emit LogChangeMPC(mpc, pendingMPC, delayMPC);
-    }
-
-    function applyMPC() external {
-        require(msg.sender == pendingMPC, "MPC: only pendingMPC");
-        require(block.timestamp >= delayMPC, "MPC: time before delayMPC");
-        emit LogApplyMPC(mpc, pendingMPC, block.timestamp);
-        mpc = pendingMPC;
-        pendingMPC = address(0);
-        delayMPC = 0;
-    }
-}
-
-// support limit operations to whitelist
-abstract contract Whitelistable is MPCManageable {
-    mapping(address => mapping(uint256 => mapping(address => bool))) public isInWhitelist;
-    mapping(address => mapping(uint256 => address[])) public whitelists;
-    mapping(address => bool) public isBlacklisted;
-
-    bool public disabled;
-
-    event LogSetWhitelist(address indexed from, uint256 indexed chainID, address indexed to, bool flag);
-
-    modifier onlyWhitelisted(address from, uint256 chainID, address[] memory to) {
-        if (!disabled) {
-            mapping(address => bool) storage map = isInWhitelist[from][chainID];
-            for (uint256 i = 0; i < to.length; i++) {
-                require(map[to[i]], "AnyCall: to address is not in whitelist");
-            }
-        }
-        _;
-    }
-
-    constructor(address _mpc) MPCManageable(_mpc) {}
-
-    function disableWhitelist() external onlyMPC {
-        disabled = true;
-    }
-
-    /**
-        @notice Query the number of elements in the whitelist of `whitelists[from][chainID]`
-        @param from The initiator of a cross chain interaction
-        @param chainID The target chain's identifier
-        @return uint256 The length of addresses `from` is allowed to call on `chainID`
-    */
-    function whitelistLength(address from, uint256 chainID) external view returns (uint256) {
-        return whitelists[from][chainID].length;
-    }
-
-    /**
-        @notice Approve/Revoke a caller's permissions to initiate a cross chain interaction
-        @param from The initiator of a cross chain interaction
-        @param chainID The target chain's identifier
-        @param to The address of the target `from` is being allowed/disallowed to call
-        @param flag Boolean denoting whether permissions is being granted/denied
-    */
-    function whitelist(address from, uint256 chainID, address to, bool flag) external onlyMPC {
-        require(isInWhitelist[from][chainID][to] != flag, "nothing change");
-        address[] storage list = whitelists[from][chainID];
-        if (flag) {
-            list.push(to);
-        } else {
-            uint256 length = list.length;
-            for (uint i = 0; i < length; i++) {
-                if (list[i] == to) {
-                    if (i + 1 < length) {
-                        list[i] = list[length-1];
-                    }
-                    list.pop();
-                    break;
-                }
-            }
-        }
-        isInWhitelist[from][chainID][to] = flag;
-        emit LogSetWhitelist(from, chainID, to, flag);
-    }
-
-    function blacklist(address account, bool flag) external onlyMPC {
-        isBlacklisted[account] = flag;
-    }
-}
-
-abstract contract Billable is Whitelistable {
-    event Fund(address indexed beneficiary, uint256 amount);
-
-    mapping(address => int256) public funds;
-    uint256 public expenses;
-
-    modifier bill(address originator) {
-        uint256 gas = gasleft();
-        _;
-        uint256 cost = (gas - gasleft()) * tx.gasprice;
-        funds[originator] -= int256(cost); // potential insufficient balances
-        expenses += cost;
-    }
-
-    constructor(address _mpc) Whitelistable(_mpc) {}
-
-    function fund(address beneficiary) external payable {
-        funds[beneficiary] += int256(msg.value);
-        emit Fund(beneficiary, msg.value);
-    }
-
-    function refundMPC() external {
-        uint256 amount = expenses;
-        if (address(this).balance >= amount) {
-            expenses = 0;
-            mpc.call{value: amount}("");
-        } else {
-            expenses = amount - address(this).balance;
-            mpc.call{value: address(this).balance}("");
-        }
-    }
-}
-
-contract AnyCallProxy is Billable {
-    event LogAnyCall(address indexed from, address[] to, bytes[] data,
-                     address[] callbacks, uint256[] nonces, uint256 fromChainID, uint256 toChainID);
-    event LogAnyExec(address indexed from, address[] to, bytes[] data, bool[] success, bytes[] result,
-                     address[] callbacks, uint256[] nonces, uint256 fromChainID, uint256 toChainID);
-
+contract AnyCallProxy {
+    // Context information for destination chain targets
     struct Context {
         address sender;
         uint256 fromChainID;
     }
 
-    Context public context;
-
-    uint private unlocked = 1;
-    modifier lock() {
-        require(unlocked == 1, 'AnyCall: LOCKED');
-        unlocked = 0;
-        _;
-        unlocked = 1;
+    // Packed fee information (only 1 storage slot)
+    struct FeeData {
+        uint128 accruedFees;
+        uint128 premium;
     }
 
-    constructor(address _mpc) Billable(_mpc) {}
+    // Packed MPC transfer info (only 1 storage slot)
+    struct TransferData {
+        uint96 effectiveTime;
+        address pendingMPC;
+    }
 
-    // @notice Query the chainID of this contract
-    // @dev Implemented as a view function so it is less expensive. CHAINID < PUSH32
-    function cID() external view returns (uint256) {
-        return block.chainid;
+    // Extra cost of execution (SSTOREs.SLOADs,ADDs,etc..)
+    // TODO: analysis to verify the correct overhead gas usage
+    uint256 constant EXECUTION_OVERHEAD = 100000;
+    // Delay for ownership transfer
+    uint256 constant TRANSFER_DELAY = 2 days;
+
+    address public mpc;
+    TransferData private _transferData;
+
+    mapping(address => bool) public blacklist;
+    mapping(address => mapping(address => mapping(uint256 => bool))) public whitelist;
+
+    Context public context;
+
+    mapping(address => uint256) public executionBudget;
+    FeeData private _feeData;
+
+    event AnyCall(
+        address indexed from,
+        address indexed to,
+        bytes data,
+        address _fallback,
+        uint256 indexed toChainID
+    );
+
+    event AnyExec(
+        address indexed from,
+        address indexed to,
+        bytes data,
+        bool success,
+        bytes result,
+        address _fallback,
+        uint256 indexed fromChainID
+    );
+
+    event Deposit(address indexed account, uint256 amount);
+    event Withdrawl(address indexed account, uint256 amount);
+    event SetBlacklist(address indexed account, bool flag);
+    event SetWhitelist(
+        address indexed from,
+        address indexed to,
+        uint256 indexed toChainID,
+        bool flag
+    );
+    event TransferMPC(address oldMPC, address newMPC, uint256 effectiveTime);
+    event UpdatePremium(uint256 oldPremium, uint256 newPremium);
+
+    constructor(address _mpc, uint128 _premium) {
+        mpc = _mpc;
+        _feeData.premium = _premium;
+
+        emit TransferMPC(address(0), _mpc, block.timestamp);
+        emit UpdatePremium(0, _premium);
+    }
+
+    /// @dev Access control function
+    modifier onlyMPC() {
+        require(msg.sender == mpc); // dev: only MPC
+        _;
+    }
+
+    /// @dev Charge an account for execution costs on this chain
+    /// @param _from The account to charge for execution costs
+    modifier charge(address _from) {
+        uint256 gasUsed = gasleft() + EXECUTION_OVERHEAD;
+        _;
+        uint256 totalCost = (gasUsed - gasleft()) * (tx.gasprice + _feeData.premium);
+
+        executionBudget[_from] -= totalCost;
+        _feeData.accruedFees += uint128(totalCost);
     }
 
     /**
-        @notice Trigger a cross-chain contract interaction
-        @param to - list of addresses to call
-        @param data - list of data payloads to send / call
-        @param callbacks - the callbacks on the fromChainID to call
-        `callback(address to, bytes data, uint256 nonces, uint256 fromChainID, bool success, bytes result)`
-        @param nonces - the nonces (ordering) to include for the resulting callback
-        @param toChainID - the recipient chain that will receive the events
+        @notice Submit a request for a cross chain interaction
+        @param _to The target to interact with on `_toChainID`
+        @param _data The calldata supplied for the interaction with `_to`
+        @param _fallback The address to call back on the originating chain
+            if the cross chain interaction fails
+        @param _toChainID The target chain id to interact with
     */
     function anyCall(
-        address[] memory to,
-        bytes[] memory data,
-        address[] memory callbacks,
-        uint256[] memory nonces,
-        uint256 toChainID
-    ) external onlyWhitelisted(msg.sender, toChainID, to) {
-        // require(toChainID != block.chainid, "AnyCall: FORBID");
-        require(!isBlacklisted[msg.sender]);
-        emit LogAnyCall(msg.sender, to, data, callbacks, nonces, block.chainid, toChainID);
+        address _to,
+        bytes calldata _data,
+        address _fallback,
+        uint256 _toChainID
+    ) external {
+        require(!blacklist[msg.sender]); // dev: caller is blacklisted
+        require(whitelist[msg.sender][_to][_toChainID]); // dev: request denied
+
+        emit AnyCall(msg.sender, _to, _data, _fallback, _toChainID);
     }
 
-    function anyCall(
-        address from,
-        address[] memory to,
-        bytes[] memory data,
-        address[] memory callbacks,
-        uint256[] memory nonces,
-        uint256 fromChainID
-    ) external onlyMPC lock bill(from) {
-        require(from != address(this) && from != address(0), "AnyCall: FORBID");
-        require(!isBlacklisted[from]);
+    /**
+        @notice Execute a cross chain interaction
+        @dev Only callable by the MPC
+        @param _from The request originator
+        @param _to The cross chain interaction target
+        @param _data The calldata supplied for interacting with target
+        @param _fallback The address to call on `_fromChainID` if the interaction fails
+        @param _fromChainID The originating chain id
+    */
+    function anyExec(
+        address _from,
+        address _to,
+        bytes calldata _data,
+        address _fallback,
+        uint256 _fromChainID
+    ) external charge(_from) onlyMPC {
+        context = Context({sender: _from, fromChainID: _fromChainID});
+        (bool success, bytes memory result) = _to.call(_data);
+        context = Context({sender: address(0), fromChainID: 0});
 
-        uint256 length = to.length;
-        bool[] memory success = new bool[](length);
-        bytes[] memory results = new bytes[](length);
+        emit AnyExec(_from, _to, _data, success, result, _fallback, _fromChainID);
 
-        context = Context({sender: from, fromChainID: fromChainID});
-
-        for (uint256 i = 0; i < length; i++) {
-            address _to = to[i];
-            if (isInWhitelist[from][block.chainid][_to]) {
-                (success[i], results[i]) = _to.call{value:0}(data[i]);
-            } else {
-                (success[i], results[i]) = (false, "forbid calling");
-            }
+        // Call the fallback on the originating chain with the call information (to, data)
+        // _from, _fromChainID, _toChainID can all be identified via contextual info
+        if (!success && _fallback != address(0)) {
+            emit AnyCall(
+                _from,
+                _fallback,
+                abi.encodeWithSignature("anyFallback(address,bytes)", _to, _data),
+                address(0),
+                _fromChainID
+            );
         }
-        context = Context({sender: from, fromChainID: 0});
-        emit LogAnyExec(from, to, data, success, results, callbacks, nonces, fromChainID, block.chainid);
+    }
+
+    /// @notice Deposit native currency crediting `_account` for execution costs on this chain
+    /// @param _account The account to deposit and credit for
+    function deposit(address _account) external payable {
+        executionBudget[_account] += msg.value;
+        emit Deposit(_account, msg.value);
+    }
+
+    /// @notice Withdraw a previous deposit from your account
+    /// @param _amount The amount to withdraw from your account
+    function withdraw(uint256 _amount) external {
+        executionBudget[msg.sender] -= _amount;
+        emit Withdrawl(msg.sender, _amount);
+        msg.sender.call{value: _amount}("");
+    }
+
+    /// @notice Withdraw all accrued execution fees
+    /// @dev The MPC is credited in the native currency
+    function withdrawAccruedFees() external {
+        uint256 fees = _feeData.accruedFees;
+        _feeData.accruedFees = 0;
+        mpc.call{value: fees}("");
+    }
+
+    /// @notice Set the whitelist premitting an account to issue a cross chain request
+    /// @param _from The account which will submit cross chain interaction requests
+    /// @param _to The target of the cross chain interaction
+    /// @param _toChainID The target chain id
+    function setWhitelist(
+        address _from,
+        address _to,
+        uint256 _toChainID,
+        bool _flag
+    ) external onlyMPC {
+        whitelist[_from][_to][_toChainID] = _flag;
+        emit SetWhitelist(_from, _to, _toChainID, _flag);
+    }
+
+    /// @notice Set an account's blacklist status
+    /// @dev A simpler way to deactive an account's permission to issue
+    ///     cross chain requests without updating the whitelist
+    /// @param _account The account to update blacklist status of
+    /// @param _flag The blacklist state to put `_account` in
+    function setBlacklist(address _account, bool _flag) external onlyMPC {
+        blacklist[_account] = _flag;
+        emit SetBlacklist(_account, _flag);
+    }
+
+    /// @notice Set the premimum for cross chain executions
+    /// @param _premium The premium per gas
+    function setPremium(uint128 _premium) external onlyMPC {
+        emit UpdatePremium(_feeData.premium, _premium);
+        _feeData.premium = _premium;
+    }
+
+    /// @notice Initiate a transfer of MPC status
+    /// @param _newMPC The address of the new MPC
+    function changeMPC(address _newMPC) external onlyMPC {
+        _transferData = TransferData({
+            effectiveTime: uint96(block.timestamp + TRANSFER_DELAY),
+            pendingMPC: _newMPC
+        });
+        emit TransferMPC(mpc, _newMPC, block.timestamp + TRANSFER_DELAY);
+    }
+
+    /// @notice Finalize the MPC transfer
+    /// @dev Only callable by the pending MPC and only after effective time
+    function applyMPC() external {
+        require(msg.sender == _transferData.pendingMPC); // dev: only pending MPC
+        require(block.timestamp >= _transferData.effectiveTime); // dev: too early
+
+        mpc = msg.sender;
+    }
+
+    /// @notice Get the total accrued fees in native currency
+    /// @dev Fees increase when executing cross chain requests
+    function accruedFees() external view returns(uint128) {
+        return _feeData.accruedFees;
+    }
+
+    /// @notice Get the gas premium cost
+    /// @dev This is similar to priority fee in eip-1559, except instead of going
+    ///     to the miner it is given to the MPC executing cross chain requests
+    function premium() external view returns(uint128) {
+        return _feeData.premium;
+    }
+
+    /// @notice Get the effective time at which pendingMPC may become MPC
+    function effectiveTime() external view returns(uint256) {
+        return _transferData.effectiveTime;
+    }
+
+    /// @notice Get the address of the pending MPC
+    function pendingMPC() external view returns(address) {
+        return _transferData.pendingMPC;
     }
 }
