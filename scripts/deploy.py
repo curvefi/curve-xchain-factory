e213263ee9be9b8b17549b3610c2cf732dd68a49
Roman Agureev <Agureev.RS@phystech.edu>
2024-08-02 13:29:34 +0300

    chore: update deploy script

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,36 +1,45 @@
 from brownie import (
-    ChildGauge,
-    ChildGaugeFactory,
-    ChildOracle,
     RootGauge,
+    ChildGauge,
+    ZERO_ADDRESS,
+    ChildGaugeFactory,
     RootGaugeFactory,
-    RootOracle,
+    RootGaugeFactoryProxy,
     accounts,
 )
 
-DEPLOYER = accounts.load("xchain-deployer")
+DEPLOYER = accounts.load("xchain_deployer", password="Xorg")
+txparams = {"from": DEPLOYER, "priority_fee": "1 gwei", "max_fee": "20 gwei"}
 
 CRV = "0xD533a949740bb3306d119CC777fa900bA034cd52"
 GAUGE_CONTROLLER = "0x2F50D538606Fa9EDD2B11E2446BEb18C9D5846bB"
 MINTER = "0xd061D61a4d941c39E5453435B6345Dc261C2fcE0"
 VE = "0x5f3b5DfEb7B28CDbD7FAba78963EE202a494e2A2"
 
-ANYCALL = "0x37414a8662bc1d25be3ee51fb27c2686e2490a89"
+ANYCALL = ZERO_ADDRESS
+
+L2_CRV20 = ""
+# txparams = {"from": DEPLOYER}  # , "gas_price": "150 gwei"}
 
 
 def deploy_root():
-    factory = RootGaugeFactory.deploy(ANYCALL, DEPLOYER, {"from": DEPLOYER})
-    gauge_impl = RootGauge.deploy(CRV, GAUGE_CONTROLLER, MINTER, {"from": DEPLOYER})
-    RootOracle.deploy(factory, VE, ANYCALL, {"from": DEPLOYER})
+    factory = RootGaugeFactory.deploy(ANYCALL, DEPLOYER, txparams)
+    gauge_impl = RootGauge.deploy(CRV, GAUGE_CONTROLLER, MINTER, txparams)
 
     # set implementation
-    factory.set_implementation(gauge_impl, {"from": DEPLOYER})
+    factory.set_implementation(gauge_impl, txparams)
+
+    # bridger = OptimismBridger.deploy(L2_CRV20, L1_BRIDGE, txparams)
+    # factory.set_child(5000, bridger, ...txparams)
+
+    proxy = RootGaugeFactoryProxy.deploy(txparams)
+    factory.commit_transfer_ownership(proxy, txparams)
+    proxy.accept_transfer_ownership(factory, txparams)
 
 
-def deploy_child(crv_addr):
-    factory = ChildGaugeFactory.deploy(ANYCALL, crv_addr, DEPLOYER, {"from": DEPLOYER})
-    gauge_impl = ChildGauge.deploy(crv_addr, factory, {"from": DEPLOYER})
-    ChildOracle.deploy(ANYCALL, {"from": DEPLOYER})
+def deploy_child(crv_addr=L2_CRV20):
+    factory = ChildGaugeFactory.deploy(ANYCALL, crv_addr, DEPLOYER, txparams)
+    gauge_impl = ChildGauge.deploy(crv_addr, factory, txparams)
 
     # set implementation
-    factory.set_implementation(gauge_impl, {"from": DEPLOYER})
+    factory.set_implementation(gauge_impl, txparams)
